2786cce2004324c86211e445e13ff7ae51d150f0
Bruno Santos <brunonascimentosantos@gmail.com>
2016-10-27 11:47:14 -0200

    simple preview, with rotation and change of camera (rear, front). Need to fix some aspect ratio issues

--- app/src/main/java/com/bnsantos/simple/camera/MainActivity.java
+++ app/src/main/java/com/bnsantos/simple/camera/MainActivity.java
@@ -1,13 +1,27 @@
 package com.bnsantos.simple.camera;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.bnsantos.camera.CameraActivity;
+import com.bnsantos.simple.camera.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+  private ActivityMainBinding mBinding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    mBinding.camera.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View v) {
+    startActivity(new Intent(this, CameraActivity.class));
   }
 }
---
+++ camera/src/main/java/com/bnsantos/camera/CameraActivity.java
@@ -0,0 +1,26 @@
+package com.bnsantos.camera;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.bnsantos.camera.view.Camera2Fragment;
+
+public class CameraActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_camera);
+    if(savedInstanceState == null){
+      if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+          getFragmentManager().beginTransaction()
+              .replace(R.id.container, Camera2Fragment.newInstance())
+              .commit();
+      }else{
+        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show();
+      }
+    }
+  }
+}
